26df5fe1fd2baac3474dd242abf1d603a56e10c4
mirajHossen99 <mirajhossen211013@gmail.com>
2026-08-12 00:45:58 +0600

    User-SQL

--- sql/001_users.sql
+++ sql/001_users.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS users(
-    id UUID PMIMARY KEY gen_random_uuid(),
-    
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL DEFAULT 'USER' CHECK (role IN ('USER', 'ADMIN')),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 )
